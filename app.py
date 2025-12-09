3bc0fd79aa4912a16e67f025dc43e37672fa1c09
Hossein <hosseinekhodadadi@gmail.com>
2025-12-09 23:22:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "The vercel is working"
+
 @app.route("api")
 def api():
-    return "This is vercel api"
+    return "This is vercel api that you can see"
+
+@app.route("project 1")
+def project():
+    return "This is project one"
 
 if __name__ == "__main__":
     app.run()
