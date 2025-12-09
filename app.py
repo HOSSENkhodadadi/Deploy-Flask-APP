fc92b4dd0c5b439d95e428513d96b6a517be5807
Hossein <hosseinekhodadadi@gmail.com>
2025-12-09 23:15:58 +0100

    add get to the app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "The vercel is working"
+@app.route("api")
+def api():
+    return "This is vercel api"
 
 if __name__ == "__main__":
     app.run()
